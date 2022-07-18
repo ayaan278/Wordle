7115457823d764c51a817bf401704c2a10c7458e
Ayaan Ahmad <ayaanishere08@gmail.com>
2022-07-18 18:39:19 +0800

    added word bank

--- wordle/src/App.js
+++ wordle/src/App.js
@@ -1,11 +1,30 @@
+import { useEffect, useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import {
+  Heading
+} from '@chakra-ui/react';
+
 
 function App() {
+
+  const [solution, setSolution] = useState(null);
+  useEffect(() => {
+    fetch('http://localhost:3001/solutions')
+      .then(res => res.json())
+      .then(json => {
+        // console.log(json);
+        //random integer from json file
+        const randomSolution = json[Math.floor(Math.random()*json.length)]
+        setSolution(randomSolution.word)
+      })
+  }, [setSolution]);
+
   return (
     <>
       <Header/>
-        
+        {solution && <Heading>{solution}</Heading>} 
+        {/* doesn't print solution unless there is some values to it */}
       <Footer/>
     </>
   );
