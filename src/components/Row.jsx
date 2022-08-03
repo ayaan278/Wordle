2d09d2c36eae32764220ada5196bd38cad1f5647
Ayaan Ahmad <ayaanishere08@gmail.com>
2022-08-03 15:02:12 +0800

    add: deploy

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {  HStack, PinInput, PinInputField } from "@chakra-ui/react";
 // import ReactCardFlip from 'react-card-flip';
+import '../styles/keyframes.css'
 
 export default function Row({ guess, currentGuess }) {
 
@@ -9,7 +10,7 @@ export default function Row({ guess, currentGuess }) {
         <HStack py="0.25em" >
           {guess.map((l, i) => (
             <PinInput size={{ base: "md", md: "md", lg: "lg" }} defaultValue={l.key.toUpperCase()} variant={'filled'} >
-              <PinInputField key={i} bg={l.color} color="white" fontSize={'24px'} fontWeight={'600'}/>
+              <PinInputField key={i} bg={l.color} color="white" fontSize={'24px'} fontWeight={'600'} animation={'swing-in-top-fwd .6s cubic-bezier(.39,.575,.565,1.000) both'}/>
             </PinInput>
           ))}
         </HStack>
@@ -24,12 +25,12 @@ export default function Row({ guess, currentGuess }) {
       <HStack py="0.25em">
         {letters.map((letter, i) => (
             <PinInput size={{ base: "md", md: "md", lg: "lg" }} defaultValue={letter} variant={'filled'} >
-              <PinInputField key={i} bg={letter.color} ontSize={'24px'}/>
+              <PinInputField key={i} bg={letter.color} ontSize={'24px'} />
             </PinInput>
           ))}
           {[...Array(7 - letters.length)].map((_, i) => (
             <PinInput size={{ base: "md", md: "md", lg: "lg" }} defaultValue="     " variant={'filled'}>
-              <PinInputField key={i} ontSize={'24px'}/>
+              <PinInputField key={i} ontSize={'24px'} />
             </PinInput>
           ))}
       </HStack>
--- src/components/Wordle.jsx
+++ src/components/Wordle.jsx
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Center, Container, Divider, Heading, Stack, VStack, } from "@chakra-ui/react";
+import {
+  Center,
+  Container,
+  Divider,
+  Heading,
+  Stack,
+  VStack,
+} from "@chakra-ui/react";
 import useWordle from "../hooks/useWordle";
 import Grid from "./Grid";
 import Keypad from "./Keypad";
@@ -7,48 +14,63 @@ import GameOver from "./GameOver";
 import Hint from "./Hint";
 
 export default function Worlde({ solution, hint }) {
-  const { currentGuess, guesses, turn, isCorrect, usedKeys, handleKeyup } = useWordle(solution);
-  const [showModal, setShowModal] = useState(false)
-  const [hints, showHints] = useState(false)
+  const { currentGuess, guesses, turn, isCorrect, usedKeys, handleKeyup } =
+    useWordle(solution);
+  const [showModal, setShowModal] = useState(false);
+  const [hints, showHints] = useState(false);
 
   useEffect(() => {
-    window.addEventListener("keyup", handleKeyup)
-    
-    if(isCorrect){
-        // setTimeout(() => setShowModal(true), 2000);
-        setShowModal(true)
-        window.removeEventListener("keyup", handleKeyup)
+    window.addEventListener("keyup", handleKeyup);
+
+    if (isCorrect) {
+      // setTimeout(() => setShowModal(true), 2000);
+      setShowModal(true);
+      window.removeEventListener("keyup", handleKeyup);
     }
 
-    if(turn > 5){
-        // setTimeout(() => setShowModal(true), 2000);
-        setShowModal(true)
-        window.removeEventListener("keyup", handleKeyup)
+    if (turn > 5) {
+      // setTimeout(() => setShowModal(true), 2000);
+      setShowModal(true);
+      window.removeEventListener("keyup", handleKeyup);
     }
-    if(turn > 3 && isCorrect===false){
-      showHints(true)
+    if (turn > 3 && isCorrect === false) {
+      showHints(true);
     }
-    return () => window.removeEventListener("keyup", handleKeyup)
-  }, [handleKeyup, isCorrect, turn]) //passing dependencies so it changes in the change of event
+    return () => window.removeEventListener("keyup", handleKeyup);
+  }, [handleKeyup, isCorrect, turn]); //passing dependencies so it changes in the change of event
 
   return (
-    <Center py="3em" bg="#00C9C8" overflowX={'hidden'}>
+    <Center py="3em" bg="#00C9C8" overflowX={"hidden"}>
       <Container>
         <VStack>
-            {/* <Heading> Solution - {solution}</Heading> */}
-            <Stack direction={{base:'column', md: 'column' , lg: 'row'}}>
-              <VStack pos={{base: "static", md:"absolute", lg:"absolute"}} top="200" left={{base: '0', md: '40', lg: '50'}} >
-                {hints && <Hint hint={hint} />}
-              </VStack>
-              <VStack w="100%" >
-                <Grid currentGuess={currentGuess} guesses={guesses} turn={turn}  />
-              </VStack>
-              <VStack pos={{base: "static", md:"static", lg:"absolute"}} top="120" right="50">
-                {showModal && <GameOver isCorrect={isCorrect} turn={turn} solution={solution} />}
-              </VStack>
-            </Stack>
-            <Divider/>
-            <Keypad usedKeys={usedKeys}/>
+          {/* <Heading> Solution - {solution}</Heading> */}
+          <Stack direction={{ base: "column", md: "column", lg: "row" }}>
+            <VStack
+              pos={{ base: "static", md: "absolute", lg: "absolute" }}
+              top="200"
+              left={{ base: "0", md: "40", lg: "50" }}
+            >
+              {hints && <Hint hint={hint} />}
+            </VStack>
+            <VStack w="100%">
+              <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
+            </VStack>
+            <VStack
+              pos={{ base: "static", md: "static", lg: "absolute" }}
+              top="120"
+              right="50"
+            >
+              {showModal && (
+                <GameOver
+                  isCorrect={isCorrect}
+                  turn={turn}
+                  solution={solution}
+                />
+              )}
+            </VStack>
+          </Stack>
+          <Divider />
+          <Keypad usedKeys={usedKeys} />
         </VStack>
       </Container>
     </Center>
