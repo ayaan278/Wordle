8dcb193db253a6b00a7d37134855d2c77bb9aba3
Ayaan Ahmad <ayaanishere08@gmail.com>
2022-08-03 15:43:35 +0800

    bug fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-import { HStack, VStack, Heading ,Popover,
+import {
+  HStack,
+  VStack,
+  Heading,
+  Popover,
   PopoverTrigger,
   PopoverContent,
   PopoverHeader,
@@ -7,60 +11,84 @@ import { HStack, VStack, Heading ,Popover,
   Divider,
   PopoverArrow,
   PopoverCloseButton,
-  PopoverAnchor, Button, Link, PopoverFooter} from "@chakra-ui/react";
-  import { FaQuestion } from "react-icons/fa";
-  import { GiBinoculars} from "react-icons/gi";
+  PopoverAnchor,
+  Button,
+  Link,
+  PopoverFooter,
+} from "@chakra-ui/react";
+import { FaQuestion } from "react-icons/fa";
+import { GiBinoculars } from "react-icons/gi";
 const Header = () => {
   return (
     <HStack p="1em" bg="#000000" color="#00C9C8">
-     
-      <VStack w={{base: '60%', md: '50%', lg: '70%'}}>
-        <Heading textAlign={'center'}>Wordle</Heading>
+      <VStack w={{ base: "60%", md: "50%", lg: "70%" }}>
+        <Heading textAlign={"center"}>Wordle</Heading>
       </VStack>
-      <HStack w={{base: '40%', md: '50%', lg: '30%'}} justifyContent={'end'}>
-        <span size='md'>
+      <HStack w={{ base: "40%", md: "50%", lg: "30%" }} justifyContent={"end"}>
+        <span size="md">
           <Popover>
             <PopoverTrigger>
-              <Button color={'#00C9C8'} bg="black" size='lg' borderRadius={'50%'} >
-                <FaQuestion/>
+              <Button
+                color={"#00C9C8"}
+                bg="black"
+                size="lg"
+                borderRadius={"50%"}
+              >
+                <FaQuestion />
               </Button>
             </PopoverTrigger>
-            <PopoverContent color={'black'}>
+            <PopoverContent color={"black"}>
               <PopoverArrow />
-              <PopoverCloseButton fontSize="22px" m="0.2em"/>
+              <PopoverCloseButton fontSize="22px" m="0.2em" />
               <PopoverHeader fontSize="26px">How to Play?</PopoverHeader>
-              <PopoverBody fontSize="18px"  textAlign="center">
+              <PopoverBody fontSize="18px" textAlign="center">
                 Guess the Word in 6 tries.<br></br>
-                Each guess must be a 7-letter word. Hit the enter button to submit after completing the word.<br></br>
-                After each guess, the color of the tiles will change to show how close your guess was to the word.
-                <Divider/>
-                The Green color tiles show the letter to be at right spot.<br></br>
-                The Yellow color tiles show the letter is present in the word but it is not placed at correct spot.<br></br>
+                Each guess must be a 7-letter word. Hit the enter button to
+                submit after completing the word.<br></br>
+                After each guess, the color of the tiles will change to show how
+                close your guess was to the word.
+                <Divider />
+                The Green color tiles show the letter to be at right spot.
+                <br></br>
+                The Yellow color tiles show the letter is present in the word
+                but it is not placed at correct spot.<br></br>
                 The Gray color tiles show the letter is not present in the word.
               </PopoverBody>
-              <PopoverFooter fontSize="18px"  textAlign="center">
-                There are a hints provide for each word that'll only appear after 4th failed attempts.<br></br>
+              <PopoverFooter fontSize="18px" textAlign="center">
+                There are a hints provide for each word that'll only appear
+                after 4th failed attempts.<br></br>
                 Good Luck and Have fun.<br></br>
               </PopoverFooter>
             </PopoverContent>
           </Popover>
         </span>
-        <span size='lg' >
+        <span size="lg">
           <Popover>
             <PopoverTrigger>
-              <Button color={'#00C9C8'} bg="black" size='lg' borderRadius={'50%'} >
-                <GiBinoculars/>
+              <Button
+                color={"#00C9C8"}
+                bg="black"
+                size="lg"
+                borderRadius={"50%"}
+              >
+                <GiBinoculars />
               </Button>
             </PopoverTrigger>
-            <PopoverContent color={'black'}>
+            <PopoverContent color={"black"}>
               <PopoverArrow />
-              <PopoverCloseButton fontSize="22px" m="0.2em"/>
+              <PopoverCloseButton fontSize="22px" m="0.2em" />
               <PopoverHeader fontSize="26px">Inspired by</PopoverHeader>
               <PopoverBody fontSize="18px" textAlign="center">
-                This Seven Letters Wordle Game is the Clone of the Official Wordle game, 
-                which is developed by Welsh software engineer Josh Wardle, and owned and published by The New York Times Company since 2022.
-                <Divider/>
-                The link to the game is<Link href="https://www.nytimes.com/games/wordle/index.html"> here</Link>
+                This Seven Letters Wordle Game is the Clone of the Official
+                Wordle game, which is developed by Welsh software engineer Josh
+                Wardle, and owned and published by The New York Times Company
+                since 2022.
+                <Divider />
+                The link to the game is
+                <Link href="https://www.nytimes.com/games/wordle/index.html">
+                  {" "}
+                  here
+                </Link>
               </PopoverBody>
             </PopoverContent>
           </Popover>
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,53 +1,79 @@
 import React from "react";
-import {  HStack, PinInput, PinInputField } from "@chakra-ui/react";
+import { HStack, PinInput, PinInputField } from "@chakra-ui/react";
 // import ReactCardFlip from 'react-card-flip';
-import '../styles/keyframes.css'
+import "../styles/keyframes.css";
 
 export default function Row({ guess, currentGuess }) {
-
   if (guess) {
     return (
-        <HStack py="0.25em" >
-          {guess.map((l, i) => (
-            <PinInput size={{ base: "md", md: "md", lg: "lg" }} defaultValue={l.key.toUpperCase()} variant={'filled'} >
-              <PinInputField key={i} bg={l.color} color="white" fontSize={'24px'} fontWeight={'600'} animation={'swing-in-top-fwd .6s cubic-bezier(.39,.575,.565,1.000) both'}/>
-            </PinInput>
-          ))}
-        </HStack>
+      <HStack py="0.25em">
+        {guess.map((l, i) => (
+          <PinInput
+            type="alphanumeric"
+            size={{ base: "md", md: "md", lg: "lg" }}
+            defaultValue={l.key.toUpperCase()}
+            variant={"filled"}
+          >
+            <PinInputField
+              key={i}
+              bg={l.color}
+              color="white"
+              fontSize={"24px"}
+              fontWeight={"600"}
+              animation={
+                "swing-in-top-fwd .6s cubic-bezier(.39,.575,.565,1.000) both"
+              }
+            />
+          </PinInput>
+        ))}
+      </HStack>
     );
   }
 
-  if(currentGuess){
+  if (currentGuess) {
+    let letters = currentGuess.split("");
 
-    let letters = currentGuess.split('');
-
-    return(
+    return (
       <HStack py="0.25em">
         {letters.map((letter, i) => (
-            <PinInput size={{ base: "md", md: "md", lg: "lg" }} defaultValue={letter} variant={'filled'} >
-              <PinInputField key={i} bg={letter.color} ontSize={'24px'} />
-            </PinInput>
-          ))}
-          {[...Array(7 - letters.length)].map((_, i) => (
-            <PinInput size={{ base: "md", md: "md", lg: "lg" }} defaultValue="     " variant={'filled'}>
-              <PinInputField key={i} ontSize={'24px'} />
-            </PinInput>
-          ))}
+          <PinInput
+            size={{ base: "md", md: "md", lg: "lg" }}
+            type="alphanumeric"
+            defaultValue={letter}
+            variant={"filled"}
+          >
+            <PinInputField key={i} bg={letter.color} ontSize={"24px"} />
+          </PinInput>
+        ))}
+        {[...Array(7 - letters.length)].map((_, i) => (
+          <PinInput
+            size={{ base: "md", md: "md", lg: "lg" }}
+            defaultValue="     "
+            variant={"filled"}
+          >
+            <PinInputField key={i} ontSize={"24px"} />
+          </PinInput>
+        ))}
       </HStack>
-    )
+    );
   }
 
   return (
-      <HStack py="0.25em">
-        <PinInput size={{ base: "md", md: "md", lg: "lg" }} defaultValue="       "  variant={'filled'} >
-          <PinInputField />
-          <PinInputField />
-          <PinInputField />
-          <PinInputField />
-          <PinInputField />
-          <PinInputField />
-          <PinInputField />
-        </PinInput>
-      </HStack>
+    <HStack py="0.25em">
+      <PinInput
+        size={{ base: "md", md: "md", lg: "lg" }}
+        defaultValue="       "
+        variant={"filled"}
+        type="alphanumeric"
+      >
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+      </PinInput>
+    </HStack>
   );
 }
